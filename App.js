dbffece1ca8d935e4b63d5cf095d7ee3c3e67dfb
Joni Hackzell <t1hajo05@students.oamk.fi>
2023-10-02 19:00:29 +0300

    etu ja sukunimi

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {DATA} from './Data';
+import Row from './components/Row';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem = {({item}) => (
+          <Row person={item} />
+        )}
+
+        ></FlatList>
+    </SafeAreaView>
   );
 }
-
+/*function renderItem({item}){
+  return (<Text>{item.lastname}</Text>);
+}
+const renderItem = ({item}) => (
+  <Text>{item.lastname}</Text>
+);*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'Pekka'
+    },
+    {
+        id: '2',
+        lastname: 'Ojala',
+        firstname: 'Lasse'
+    },
+    {
+        id: '3',
+        lastname: 'Rytky',
+        firstname: 'Raimo'
+    },
+    {
+        id: '4',
+        lastname: 'Virta',
+        firstname: 'Tanja'
+    },
+    {
+        id: '5',
+        lastname: 'Sammal',
+        firstname: 'Ilkka'
+    },
+    {
+        id: '6',
+        lastname: 'Kuusisto',
+        firstname: 'Niko'
+    },
+    {
+        id: '7',
+        lastname: 'Ojanen',
+        firstname: 'Milla'
+    },
+    {
+        id: '8',
+        lastname: 'Mäkinen',
+        firstname: 'Jukka'
+    },
+    {
+        id: '9',
+        lastname: 'Nivala',
+        firstname: 'Pekka'
+    },
+    {
+        id: '10',
+        lastname: 'Saari',
+        firstname: 'Timo'
+    },
+    {
+        id: '11',
+        lastname: 'Räme',
+        firstname: 'Olli'
+    },
+    {
+        id: '12',
+        lastname: 'Varma',
+        firstname: 'Ville'
+    },
+    {
+        id: '13',
+        lastname: 'Suomalainen',
+        firstname: 'Emmi'
+    },
+    {
+        id: '14',
+        lastname: 'Halonen',
+        firstname: 'Ida'
+    },
+    {
+        id: '15',
+        lastname: 'Erkkilä',
+        firstname: 'Matias'
+    },
+]
